b2822f7af2a9680b05eca7ef06ddcedb0153009e
Andrei Aliaksandrau <aleksandrov.a.95@yandex.ru>
2023-09-06 00:09:32 +0300

    comment added in ex4

--- src/main/java/by/aleksandrov/exceptions/ex1/Main.java
+++ src/main/java/by/aleksandrov/exceptions/ex1/Main.java
@@ -8,6 +8,6 @@ public class Main {
 
         do {
             result = floatScanner.init();
-        } while (result == -1);
+        } while (result == -1); // метод init() возвращает -1 в качетсве кода ошибки при появлении исключения
     }
 }
